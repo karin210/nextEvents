ab3c5d4592648e672bf7cb4bc44be2b96a3d1b5b
Karlos <karin_ar72@hotmail.com>
2023-05-27 11:21:01 -0600

    update: render events

--- app/page.js
+++ app/page.js
@@ -1,6 +1,29 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <main className={styles.main}></main>;
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/events");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  return (
+    <main>
+      <article>
+        {data.map((item) => (
+          <>
+            <h2>{item.id}</h2>
+            <img className={styles.img} src={item.image} alt={item.id} />
+          </>
+        ))}
+      </article>
+    </main>
+  );
 }
