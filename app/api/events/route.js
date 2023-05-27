7d080b5542ec88cbf64fed722f0b0d6875b61963
Karlos <karin_ar72@hotmail.com>
2023-05-26 23:00:48 -0600

    chore: set the database

---
+++ app/api/events/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import clientPromise from "@/app/utils/mongodb";
+
+export async function GET() {
+  const client = await clientPromise;
+  const db = client.db("eventsPage");
+  if (client) {
+    const data = await db.collection("events").find({}).toArray();
+    if (data) {
+      return NextResponse.json(data);
+    }
+    return res.status(404).send({ message: "Data not found" });
+  }
+}
