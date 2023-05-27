5ca9a5ef7c5f9036887ea053c1ad6c4148d22435
Karlos <karin_ar72@hotmail.com>
2023-05-27 14:50:59 -0600

    update: define a url for production and development stage

--- app/page.js
+++ app/page.js
@@ -2,9 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 async function getData() {
-  const res = await fetch(
-    "next-events-dcmwtrfmf-karin210.vercel.app/api/events/"
-  );
+  let url;
+  if (process.env.NODE_ENV === "production") {
+    url = "next-events-dcmwtrfmf-karin210.vercel.app";
+  } else {
+    url = "http://localhost:3000";
+  }
+  const res = await fetch(`${url}/api/events/`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
