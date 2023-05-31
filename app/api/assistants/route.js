b9b042d8580f61e9e66ae5f0cdb78a0629f9cb76
Karlos <karin_ar72@hotmail.com>
2023-05-31 12:02:58 -0600

    update: add get method to assistants api

--- app/api/assistants/route.js
+++ app/api/assistants/route.js
@@ -11,33 +11,68 @@ export async function POST(req, res) {
     console.log(exist);
     if (!exist) {
       await db.collection("assistants").insertOne(json);
-      return NextResponse.json(
-        {
-          message: "Successful registration. Enjoy your event!",
-          status: 201,
-        },
-        {
-          headers: {
-            "Access-Control-Allow-Origin":
-              "https://next-events-hyppouicf-karin210.vercel.app",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-          },
-        }
-      );
+      return NextResponse.json({
+        message: "Successful registration. Enjoy your event!",
+        status: 201,
+      });
     } else {
-      return NextResponse.json(
-        {
-          message: "email already registered!",
-          status: 504,
-        },
-        {
-          headers: {
-            "Access-Control-Allow-Origin":
-              "https://next-events-hyppouicf-karin210.vercel.app",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-          },
-        }
-      );
+      return NextResponse.json({
+        message: "email already registered!",
+        status: 504,
+      });
     }
   }
 }
+
+export async function GET() {
+  const client = await clientPromise;
+  const db = client.db("eventsPage");
+  if (client) {
+    const data = await db.collection("assistants").find({}).toArray();
+    if (data) {
+      return NextResponse.json(data);
+    }
+    return res.status(404).send({ message: "Data not found" });
+  }
+}
+
+// export async function POST(req, res) {
+//   const client = await clientPromise;
+//   const db = client.db("eventsPage");
+//   const json = await req.json();
+
+//   if (client) {
+//     const exist = await db.collection("assistants").findOne(json);
+//     console.log(exist);
+//     if (!exist) {
+//       await db.collection("assistants").insertOne(json);
+//       return NextResponse.json(
+//         {
+//           message: "Successful registration. Enjoy your event!",
+//           status: 201,
+//         }
+//         // {
+//         //   headers: {
+//         //     "Access-Control-Allow-Origin":
+//         //       "https://next-events-hyppouicf-karin210.vercel.app",
+//         //     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+//         //   },
+//         // }
+//       );
+//     } else {
+//       return NextResponse.json(
+//         {
+//           message: "email already registered!",
+//           status: 504,
+//         }
+//         // {
+//         //   headers: {
+//         //     "Access-Control-Allow-Origin":
+//         //       "https://next-events-hyppouicf-karin210.vercel.app",
+//         //     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+//         //   },
+//         // }
+//       );
+//     }
+//   }
+// }
