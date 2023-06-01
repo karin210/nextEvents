6405296b6c0b2f2718c102a7482dbed62ce497f0
Karlos <karin_ar72@hotmail.com>
2023-06-01 11:36:41 -0600

    update: set homepage links to events

--- app/components/Events.js
+++ app/components/Events.js
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import styles from "./styles/Events.module.scss";
@@ -21,25 +22,27 @@ export default function Events() {
   return (
     <section className={styles.events}>
       {events.map((item) => (
-        <article className={styles.item} key={item.id}>
-          <figure className={styles.fig}>
-            <img src={item.image} alt={item.id} />
-          </figure>
-          <h2>{item.title}</h2>
-          <ul className={styles.itemDetails}>
-            <li className={styles.details}>{item.city}</li>
-            <li className={styles.rateBar}>
-              <label className={styles.details} for="popularity">
-                Ranking
-              </label>
-              <progress
-                id="popularity"
-                max="5"
-                value={item.popularity}
-              ></progress>
-            </li>
-          </ul>
-        </article>
+        <Link href={`/events/${item.id}`} key={item.id}>
+          <article className={styles.item}>
+            <figure className={styles.fig}>
+              <img src={item.image} alt={item.id} />
+            </figure>
+            <h2>{item.title}</h2>
+            <ul className={styles.itemDetails}>
+              <li className={styles.details}>{item.city}</li>
+              <li className={styles.rateBar}>
+                <label className={styles.details} for="popularity">
+                  Ranking
+                </label>
+                <progress
+                  id="popularity"
+                  max="5"
+                  value={item.popularity}
+                ></progress>
+              </li>
+            </ul>
+          </article>
+        </Link>
       ))}
     </section>
   );
